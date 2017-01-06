07fe45b3b7d8dee8a300daa72f0fc97f8b5fa11f
dengrenjie <dengrenjie31@gmail.com>
2017-01-06 16:00:12 +0800

    first version

---
+++ game.js
@@ -0,0 +1,314 @@
+function qw1(){
+	var element1= document.getElementById("q1a");
+	element1.style.display="block";
+}
+
+function qw2(){
+	var element1= document.getElementById("q2a");
+	element1.style.display="block";
+}
+function qw3(){
+	var element1= document.getElementById("q3a");
+	element1.style.display="block";
+}
+function qw4(){
+	var element1= document.getElementById("q4a");
+	element1.style.display="block";
+}
+function qw5(){
+	var element1= document.getElementById("q5a");
+	element1.style.display="block";
+}
+function qw6(){
+	var element1= document.getElementById("q6a");
+	element1.style.display="block";
+}
+function qw7(){
+	var element1= document.getElementById("q7a");
+	element1.style.display="block";
+}
+function qw8(){
+	var element1= document.getElementById("q8a");
+	element1.style.display="block";
+}
+function qw9(){
+	var element1= document.getElementById("q9a");
+	element1.style.display="block";
+}
+function quit1(){
+	var element1=document.getElementById("q1a");
+	var element2=document.getElementById("q1");
+	var element4=document.getElementById("q2")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function right1(){
+	var element1=document.getElementById("right1");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+
+function right2(){
+	var element1=document.getElementById("right2");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right3(){
+	var element1=document.getElementById("right3");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right4(){
+	var element1=document.getElementById("right4");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right5(){
+	var element1=document.getElementById("right5");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right6(){
+	var element1=document.getElementById("right6");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right7(){
+	var element1=document.getElementById("right7");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right8(){
+	var element1=document.getElementById("right8");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function right9(){
+	var element1=document.getElementById("right9");
+	element1.style.display="block";
+	var count=document.getElementById("count");
+	count.innerHTML=parseInt(count.innerHTML)+1;
+}
+function quit2(){
+	var element1=document.getElementById("q2a");
+	var element2=document.getElementById("q2");
+	var element4=document.getElementById("q3")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit3(){
+	var element1=document.getElementById("q3a");
+	var element2=document.getElementById("q3");
+	var element4=document.getElementById("q4")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit4(){
+	var element1=document.getElementById("q4a");
+	var element2=document.getElementById("q4");
+	var element4=document.getElementById("q5")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit5(){
+	var element1=document.getElementById("q5a");
+	var element2=document.getElementById("q5");
+	var element4=document.getElementById("q6")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit6(){
+	var element1=document.getElementById("q6a");
+	var element2=document.getElementById("q6");
+	var element4=document.getElementById("q7")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit7(){
+	var element1=document.getElementById("q7a");
+	var element2=document.getElementById("q7");
+	var element4=document.getElementById("q8")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+
+function quit8(){
+	var element1=document.getElementById("q8a");
+	var element2=document.getElementById("q8");
+	var element4=document.getElementById("q9")
+	element1.style.display="none";
+	element2.style.display="none";
+	element4.style.display="block";
+}
+function r1(){
+	var element1=document.getElementById("right1");
+	var element2=document.getElementById("q1");
+	var element3=document.getElementById("q2");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r2(){
+	var element1=document.getElementById("right2");
+	var element2=document.getElementById("q2");
+	var element3=document.getElementById("q3");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r3(){
+	var element1=document.getElementById("right3");
+	var element2=document.getElementById("q3");
+	var element3=document.getElementById("q4");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r4(){
+	var element1=document.getElementById("right4");
+	var element2=document.getElementById("q4");
+	var element3=document.getElementById("q5");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r5(){
+	var element1=document.getElementById("right5");
+	var element2=document.getElementById("q5");
+	var element3=document.getElementById("q6");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r6(){
+	var element1=document.getElementById("right6");
+	var element2=document.getElementById("q6");
+	var element3=document.getElementById("q7");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r7(){
+	var element1=document.getElementById("right7");
+	var element2=document.getElementById("q7");
+	var element3=document.getElementById("q8");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+function r8(){
+	var element1=document.getElementById("right8");
+	var element2=document.getElementById("q8");
+	var element3=document.getElementById("q9");
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="block";
+}
+
+function again(){
+	var element1=document.getElementById("result0");
+	var element2=document.getElementById("result1");
+	var element3=document.getElementById("result2");
+	var element4=document.getElementById("result3");
+	var element5=document.getElementById("result4");
+	var element6=document.getElementById("q1");
+	var element7=document.getElementById("q9");
+	var again=document.getElementById("count");
+    again.innerHTML=0;
+	element1.style.display="none";
+	element2.style.display="none";
+	element3.style.display="none";
+	element4.style.display="none";
+	element5.style.display="none";
+	element6.style.display="block";
+	element7.style.display="none";
+}
+
+function getpack(){
+	var element1=document.getElementById("getpack");
+	var element2=document.getElementById("result5");
+	var element3=document.getElementById("result6");
+	var element4=document.getElementById("result7");
+	var element5=document.getElementById("result8");
+	var element6=document.getElementById("result9");
+	element1.style.display="block";
+	element2.style.display="none";
+	element3.style.display="none";
+	element4.style.display="none";
+	element5.style.display="none";
+	element6.style.display="none";
+}
+
+function last(){
+	var element1=document.getElementById("q9a");
+    var element2=document.getElementById("right9");
+    element1.style.display="none";
+    element2.style.display="none";
+	var result=document.getElementById("count").innerHTML;
+	result=parseInt(result);
+	switch(result)
+	{
+		case 0:
+		var element=document.getElementById("result0");
+		element.style.display="block";
+		break;
+		case 1:
+		var element=document.getElementById("result1");
+		element.style.display="block";
+		break;
+		case 2:
+		var element=document.getElementById("result2");
+		element.style.display="block";
+		break;
+		case 3:
+		var element=document.getElementById("result3");
+		element.style.display="block";
+		break;
+		case 4:
+		var element=document.getElementById("result4");
+		element.style.display="block";
+		break;
+		case 5:
+		var element=document.getElementById("result5");
+		element.style.display="block";
+		break;
+		case 6:
+		var element=document.getElementById("result6");
+		element.style.display="block";
+		break;
+		case 7:
+		var element=document.getElementById("result7");
+		element.style.display="block";
+		break;
+		case 8:
+		var element=document.getElementById("result8");
+		element.style.display="block";
+		break;
+		case 9:
+		var element=document.getElementById("result9");
+		element.style.display="block";
+		break;
+
+	}
+}
