7c7bec87073b0ec19e524a1f764432cc95ea871d
dengrenjie <dengrenjie31@gmail.com>
2017-01-09 14:46:51 +0800

    change js code

--- game.js
+++ game.js
@@ -1,248 +1,30 @@
-function qw1(){
-	var element1= document.getElementById("q1a");
+var count1=1;
+var count2=0;
+var count3=0;
+function qw(){
+	var element1= document.getElementById("q"+count1+"a");
 	element1.style.display="block";
 }
 
-function qw2(){
-	var element1= document.getElementById("q2a");
-	element1.style.display="block";
-}
-function qw3(){
-	var element1= document.getElementById("q3a");
-	element1.style.display="block";
-}
-function qw4(){
-	var element1= document.getElementById("q4a");
-	element1.style.display="block";
-}
-function qw5(){
-	var element1= document.getElementById("q5a");
-	element1.style.display="block";
-}
-function qw6(){
-	var element1= document.getElementById("q6a");
-	element1.style.display="block";
-}
-function qw7(){
-	var element1= document.getElementById("q7a");
-	element1.style.display="block";
-}
-function qw8(){
-	var element1= document.getElementById("q8a");
-	element1.style.display="block";
-}
-function qw9(){
-	var element1= document.getElementById("q9a");
-	element1.style.display="block";
-}
-function quit1(){
-	var element1=document.getElementById("q1a");
-	var element2=document.getElementById("q1");
-	var element4=document.getElementById("q2")
+function quit(){
+	var element1=document.getElementById("q"+count1+"a");
+	var element2=document.getElementById("q"+count1);
+	var element4=document.getElementById("q"+(++count1));
+	var element3=document.getElementById("right");
 	element1.style.display="none";
 	element2.style.display="none";
 	element4.style.display="block";
+	element3.style.display="none";
 }
 
-function right1(){
-	var element1=document.getElementById("right1");
+function right(){
+	var element1=document.getElementById("right");
 	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-
-function right2(){
-	var element1=document.getElementById("right2");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right3(){
-	var element1=document.getElementById("right3");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right4(){
-	var element1=document.getElementById("right4");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right5(){
-	var element1=document.getElementById("right5");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right6(){
-	var element1=document.getElementById("right6");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right7(){
-	var element1=document.getElementById("right7");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right8(){
-	var element1=document.getElementById("right8");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function right9(){
-	var element1=document.getElementById("right9");
-	element1.style.display="block";
-	var count=document.getElementById("count");
-	count.innerHTML=parseInt(count.innerHTML)+1;
-}
-function quit2(){
-	var element1=document.getElementById("q2a");
-	var element2=document.getElementById("q2");
-	var element4=document.getElementById("q3")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit3(){
-	var element1=document.getElementById("q3a");
-	var element2=document.getElementById("q3");
-	var element4=document.getElementById("q4")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit4(){
-	var element1=document.getElementById("q4a");
-	var element2=document.getElementById("q4");
-	var element4=document.getElementById("q5")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit5(){
-	var element1=document.getElementById("q5a");
-	var element2=document.getElementById("q5");
-	var element4=document.getElementById("q6")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit6(){
-	var element1=document.getElementById("q6a");
-	var element2=document.getElementById("q6");
-	var element4=document.getElementById("q7")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit7(){
-	var element1=document.getElementById("q7a");
-	var element2=document.getElementById("q7");
-	var element4=document.getElementById("q8")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-
-function quit8(){
-	var element1=document.getElementById("q8a");
-	var element2=document.getElementById("q8");
-	var element4=document.getElementById("q9")
-	element1.style.display="none";
-	element2.style.display="none";
-	element4.style.display="block";
-}
-function r1(){
-	var element1=document.getElementById("right1");
-	var element2=document.getElementById("q1");
-	var element3=document.getElementById("q2");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r2(){
-	var element1=document.getElementById("right2");
-	var element2=document.getElementById("q2");
-	var element3=document.getElementById("q3");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r3(){
-	var element1=document.getElementById("right3");
-	var element2=document.getElementById("q3");
-	var element3=document.getElementById("q4");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r4(){
-	var element1=document.getElementById("right4");
-	var element2=document.getElementById("q4");
-	var element3=document.getElementById("q5");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r5(){
-	var element1=document.getElementById("right5");
-	var element2=document.getElementById("q5");
-	var element3=document.getElementById("q6");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r6(){
-	var element1=document.getElementById("right6");
-	var element2=document.getElementById("q6");
-	var element3=document.getElementById("q7");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r7(){
-	var element1=document.getElementById("right7");
-	var element2=document.getElementById("q7");
-	var element3=document.getElementById("q8");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
-}
-function r8(){
-	var element1=document.getElementById("right8");
-	var element2=document.getElementById("q8");
-	var element3=document.getElementById("q9");
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="block";
+	count3=++count3;
 }
 
 function again(){
-	var element1=document.getElementById("result0");
-	var element2=document.getElementById("result1");
-	var element3=document.getElementById("result2");
-	var element4=document.getElementById("result3");
-	var element5=document.getElementById("result4");
-	var element6=document.getElementById("q1");
-	var element7=document.getElementById("q9");
-	var again=document.getElementById("count");
-    again.innerHTML=0;
-	element1.style.display="none";
-	element2.style.display="none";
-	element3.style.display="none";
-	element4.style.display="none";
-	element5.style.display="none";
-	element6.style.display="block";
-	element7.style.display="none";
+	window.location.reload();
 }
 
 function getpack(){
@@ -260,13 +42,17 @@ function getpack(){
 	element6.style.display="none";
 }
 
+function right9(){
+	var element1=document.getElementById("right9");
+	element1.style.display="block";
+}
+
 function last(){
 	var element1=document.getElementById("q9a");
     var element2=document.getElementById("right9");
     element1.style.display="none";
     element2.style.display="none";
-	var result=document.getElementById("count").innerHTML;
-	result=parseInt(result);
+	var result=count3;
 	switch(result)
 	{
 		case 0:
